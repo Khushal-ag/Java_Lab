a1ac3797d69dc7804fcde1a6495cde22101cee32
Khushal_Agarwal <starkhush5@gmail.com>
2021-09-28 15:05:44 +0530

    Initial commit

---
+++ src/questions/ClassAc.java
@@ -0,0 +1,9 @@
+package questions;
+
+class Room{
+    Boolean AC_ON,HOME_THEATRE_ON,FAN_ON,LIGHT_ON;
+
+}
+public class ClassAc{
+
+}
--- src/questions/Question1.java
+++ src/questions/Question1.java
@@ -7,21 +7,24 @@
 package questions;
 class Student1{
     String stuName;
+    int rollNo;
     Student1(){
+        rollNo = 0;
         stuName = "Unknown";
     }
 
-    Student1(String name){
+    Student1(String name,int roll){
+        rollNo = roll;
         stuName = name;
     }
 }
 public class Question1 {
     public static void main(String[] args) {
-        Student1 stu1 = new Student1("Rohan");
+        Student1 stu1 = new Student1("Rohan",23);
         Student1 stu2 = new Student1();
-        Student1 stu3 = new Student1("Aman");
-        System.out.println(stu1.stuName);
-        System.out.println(stu2.stuName);
-        System.out.println(stu3.stuName);
+        Student1 stu3 = new Student1("Aman",15);
+        System.out.println(stu1.stuName+" "+stu1.rollNo);
+        System.out.println(stu2.stuName+" "+stu2.rollNo);
+        System.out.println(stu3.stuName+" "+stu3.rollNo);
     }
 }
